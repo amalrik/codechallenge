f911a56eb0e5fc0a0e9b12532a52c0cd500f9682
amalrik <amalrik.maia@gmail.com>
2014-10-05 21:26:53 -0300

    check if tables exists before use them in initializers.

--- config/initializers/metro_graph.rb
+++ config/initializers/metro_graph.rb
@@ -1,7 +1,9 @@
 Rails.application.config.before_initialize do
   Metro_Graph = Graph.new
-  Line.all.each do |line|
-    Metro_Graph.add_vertex(line.station1.to_s, {line.station2.to_s => 1})
-    Metro_Graph.add_vertex(line.station2.to_s, {line.station1.to_s => 1})
+  if ActiveRecord::Base.connection.table_exists? 'kittens'
+    Line.all.each do |line|
+      Metro_Graph.add_vertex(line.station1.to_s, {line.station2.to_s => 1})
+      Metro_Graph.add_vertex(line.station2.to_s, {line.station1.to_s => 1})
+    end
   end
 end
